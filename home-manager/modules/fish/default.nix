10b099b2b22ba7c090c2de8d009fe5246cded892
midboob <edwarddan72@gmail.com>
2025-08-14 10:04:55 -0700

    switched to zsh from fish

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -5,7 +5,7 @@
 	programs.zsh = {
 		enable = true;
 		enableCompletions = true;
-		autosuggestions.enable = true;
+		autosuggestion.enable = true;
 		syntaxHighlighting.enable = true;
 
 		shellAliases = {
