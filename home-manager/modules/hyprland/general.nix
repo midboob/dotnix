05f7c8241f39eb70cb93ab8b778a2f92bf532f6e
midboob <edwarddan72@gmail.com>
2025-08-15 10:56:42 -0700

    editted hyprland monitor settings

--- home-manager/modules/hyprland/general.nix
+++ home-manager/modules/hyprland/general.nix
@@ -1,8 +1,8 @@
 { pkgs, config, lib, ... }: {
 	wayland.windowManager.hyprland.settings = {
 		monitor = [ 
-			"DP-6,1920x1080@144,0x0,1"
-			"HDMI-A-2,1920x1080@60,1920x0,1"
+			",highrr,0x0,1"
+			"HDMI-A-2,1920x1080@70,1920x0,1"
 		];
 
 		exec-once = [
