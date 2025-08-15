480585fe5d9baedc8349d6210962523d7f64136f
midboob <edwarddan72@gmail.com>
2025-08-15 11:23:39 -0700

    final

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -15,8 +15,8 @@
 			# nrb = "nh os boot";
 			# hms = "nh home switch";
 
-			nrs = "sudo nixos-rebuild switch --flake ~/.dotnix";
-			nrb = "sudo nixos-rebuild boot --flake ~/.dotnix";
+			nrs = "sudo nixos-rebuild switch --impure --flake ~/.dotnix";
+			nrb = "sudo nixos-rebuild boot --impure --flake ~/.dotnix";
 			hms = "home-manager switch --flake ~/.dotnix";
 
 			# Nvim
--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -2,15 +2,19 @@
 
 {
 	boot = {
+    loader = {
+      grub = {
         extraEntries = ''
           menuentry "Arch Linux (manual)" {
             insmod part_gpt
             insmod fat
-          search --no-floppy --fs-uuid --set=root 3A75-2890
-          linux /vmlinuz-linux root=UUID=b9c9d8e4-b049-4ac5-a48a-c0f7a1e96603 rw quiet
+            search --no-floppy --fs-uuid --set=root 3A75-2890
+            linux /vmlinuz-linux root=UUID=b9c9d8e4-b049-4ac5-a48a-c0f7a1e96603 rw quiet
             initrd /initramfs-linux.img
             }
         '';
+      };
+    };
   };
 
   boot.supportedFilesystems = [ "ntfs" ];
