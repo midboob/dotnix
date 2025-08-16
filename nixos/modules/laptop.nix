3a04b1fac3b6d85a6e8544a2f3881d777e88ad5a
midboob <edwarddan72@gmail.com>
2025-08-16 11:35:44 -0700

    feat: ✨ added tlp to laptop.nix

--- nixos/modules/laptop.nix
+++ nixos/modules/laptop.nix
@@ -4,23 +4,34 @@
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480s
   ];
 
-  powerManagement.enable = true;
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    };
   services = {
     thermald.enable = true;
-    auto-cpufreq = {
+
+    tlp = {
       enable = true;
       settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-        };
-        charger = {
-          governor = "performance";
-          turbo = "auto";
-        };
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        # CPU_MIN_PERF_ON_AC = 0;
+        # CPU_MAX_PERF_ON_AC = 100;
+        # CPU_MIN_PERF_ON_BAT = 0;
+        # CPU_MAX_PERF_ON_BAT = 80;
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        STOP_CHARGE_THRESH_BAT0 = 95;
+
       };
     };
-    power-profiles-daemon.enable = true;
+
+    power-profiles-daemon.enable = false;
     logind.lidSwitch = "suspend-then-hibernate";
     logind.powerKey = "hibernate";
     logind.powerKeyLongPress = "poweroff";
